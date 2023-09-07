a2cd7bea5068a0cda1b73f1859d405b89d449c17
123Luka123 <abramishvililuka29@gmail.com>
2023-09-07 14:17:31 +0400

    second commit

---
+++ day 2/lesson2.py
@@ -0,0 +1,17 @@
+name = "Luka"
+
+surname = 'Abramishvili'
+
+age = 19
+
+height =  170.5
+
+knows_music = True
+
+
+is_tall = False
+
+print("my  name is " + name + " " + surname +". " 
+       " I am " + str(age)+" years old." 
+       " my height is " + str(height) + "cm."
+        )
